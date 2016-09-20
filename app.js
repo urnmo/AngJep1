2c9f529ad87e0ca7baedbc42fa9d03e90e7db41e
urnmo <aaron0moore@gmail.com>
2016-09-20 14:32:40 -0400

    some functionality

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ $scope.Uguess = 0;
 $scope.result = "";
 $scope.score = "0";
 $scope.answers = [];
-$displayButt = 0;
+let displayButt = 0;
 
 $scope.questions = [{Q: "Name the largest freshwater lake in the world?", A:"Lake Superior", score: 50}, {Q: "Where would you find the Sea of Tranquility?", A:"the Moon", score: 50}, {Q: "What kind of weapon is a falchion?", A:"a sword", score: 50}, {Q: "Name the seventh planet from the sun.", A:"Uranus", score: 50}, {Q: "Who invented the rabies vaccination?", A:"Louis Pasteur", score: 50},];
 
-$scope.currQ = $scope.questions[$displayButt].Q;
+$scope.currQ = $scope.questions[displayButt].Q;
 $scope.upDate = function(){
     console.log('working?')
+   displayButt++;
+    $scope.currQ = $scope.questions[displayButt].Q;
 
     $scope.answers.push({
-       question: $scope.questions.Q,
+       question: $scope.currQ,
        correct: 'true',
     });
 };
